139aa68a809765df6fa38001a98eb9bc3ef7c991
Roman <roman.ambroselli@gmail.com>
2016-12-13 00:42:57 -0800

    various style updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,15 @@
 angular.module('searchApp', ['ngSanitize', 'algoliasearch'])
-.controller('searchController', ['$scope', '$sce', 'algolia', '$q', function($scope, $sce, algolia, $q) {
+.controller('SearchController', ['$scope', '$sce', 'algolia', '$q', function($scope, $sce, algolia, $q) {
     var algolia = algolia.Client('BVC5SUAF7Y', 'c196706d8976847f9c55b77930e36a92');
     var hitsPerPage = 3;
-
     var latLngParam;
+    var vm = this;
+    vm.q = '';
+    vm.content = null;
+    vm.toggleRefine = toggleRefine;
+    vm.showMore = showMore;
 
-    if ("geolocation" in navigator) {
-        navigator.geolocation.getCurrentPosition(function(position) {
-            latLngParam = position.coords.latitude + ',' + position.coords.longitude;
-            $scope.helper.search();
-        });
-    } else {
-        //geolocation IS NOT available
-    }
-
-    $scope.q = '';
-    $scope.content = null;
-
-    $scope.helper = algoliasearchHelper(algolia, 'rest_search', {
+    vm.helper = algoliasearchHelper(algolia, 'rest_search', {
         facets: ['food_type','payment_options'],
         disjunctiveFacets: ['stars_count'],
         hitsPerPage: hitsPerPage,
@@ -25,13 +17,23 @@ angular.module('searchApp', ['ngSanitize', 'algoliasearch'])
         getRankingInfo: true,
         aroundLatLng: latLngParam
     });
-    $scope.helper.on('result', function(content) {
-        $scope.paymentOptions = content.getFacetValues('payment_options');
-        $scope.foodType = content.getFacetValues('food_type');
-        $scope.processTime = (content.processingTimeMS / 1000) % 60;
-        $scope.hits = content.nbHits;
-        $scope.pages = content.nbPages;
-        $scope.page = content.page + 1;
+
+    if ("geolocation" in navigator) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            latLngParam = position.coords.latitude + ',' + position.coords.longitude;
+            vm.helper.search();
+        });
+    } else {
+        //geolocation IS NOT available
+    }
+
+    vm.helper.on('result', function(content) {
+        vm.paymentOptions = content.getFacetValues('payment_options');
+        vm.foodType = content.getFacetValues('food_type');
+        vm.processTime = (content.processingTimeMS / 1000) % 60;
+        vm.hits = content.nbHits;
+        vm.pages = content.nbPages;
+        vm.page = content.page + 1;
 
         content.hits.map(function(item){
             item.starsRounded = Math.round(item.stars_count*2)/2;
@@ -39,25 +41,27 @@ angular.module('searchApp', ['ngSanitize', 'algoliasearch'])
             return item;
         });
         $scope.$apply(function() {
-            $scope.content = content;
+            vm.content = content;
         });
     });
-    $scope.toggleRefine = function($event, facet, value) {
-        $event.preventDefault();
-        $scope.helper.toggleRefine(facet, value).search();
-    };
-    $scope.showMore = function(){
-        hitsPerPage += hitsPerPage;
-        $scope.helper.state.hitsPerPage = hitsPerPage;
-        $scope.helper.search();
-        //$scope.helper.setHitsPerPage(hitsPerPage).search();
-    }
-    $scope.$watch('q', function(q) {
-        $scope.helper.setQuery(q).search();
+
+    $scope.$watch('SearchController.q', function(q) {
+        vm.helper.setQuery(q).search();
     });
 
-    $scope.helper.search();
-    //$scope.helper.removeFacetExclusion('payment_options', 'Diners Club');
+    vm.helper.search();
+
+    function toggleRefine ($event, facet, value) {
+        $event.preventDefault();
+        vm.helper.toggleRefine(facet, value).search();
+    }
+
+    function showMore(){
+        hitsPerPage += hitsPerPage;
+        vm.helper.state.hitsPerPage = hitsPerPage;
+        vm.helper.search();
+        //vm.helper.setHitsPerPage(hitsPerPage).search();
+    }
 }]);
 
 /*
